aca84271bb13448bc06abb1c3b670f8a6636a24d
0KaioGomes <kg6370694@gmail.com>
2021-06-24 20:55:39 -0300

    testando uma condição simples

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -48,6 +48,9 @@ export function AdminRoom() {
                     {questions.length > 0 && <span>{questions.length} perguntas</span>}
                 </div>
                 <div className="question-list">
+                    {questions.length > 0 && (
+                        <h1>teste</h1>
+                    )}
                     {questions.map(question => {
                         return (
                             <Question
