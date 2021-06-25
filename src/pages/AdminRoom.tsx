e264bd7e752446d62ae40754b5ab8593aabd90f7
0KaioGomes <kg6370694@gmail.com>
2021-06-25 09:38:17 -0300

    Adicionado tela de carregamento em AdminRoom

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -11,6 +11,8 @@ import { RoomCode } from '../components/RoomCode';
 import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 import '../styles/room.scss'
+import { useEffect, useState } from 'react';
+import SyncLoader from 'react-spinners/SyncLoader';
 
 type RoomParams = {
     id: string;
@@ -21,6 +23,15 @@ export function AdminRoom() {
     const params = useParams<RoomParams>();
     const roomId = params.id;
     const { title, questions } = useRoom(roomId);
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=> {
+        setLoading(true);
+        setTimeout(()=>{
+            setLoading(false);
+        }, 5000)
+    }, [])
 
     async function handleEndRoom() {
         database.ref(`rooms/${roomId}`).update({
@@ -49,63 +60,89 @@ export function AdminRoom() {
 
     return (
         <div id="page-room">
-            <header>
-                <div className="content">
-                    <img src={logoImg} alt="Letmeask"/>
-                    <div>
-                        <RoomCode code={params.id}/>
-                        <Button isOutlined onClick={handleEndRoom}>Encerrar Sala</Button>
-                    </div>
-                </div>
-            </header>
-            <main className="content">
-                <div className="room-title">
-                    <h1>Sala {title}</h1>
-                    {questions.length > 0 && <span>{questions.length} perguntas</span>}
-                </div>
-                <div className="question-list">
-                    {questions.length === 0 && (
+        {
+        loading ? 
+            <>
+                <header>
+                    <div className="content">
+                        <img src={logoImg} alt="Letmeask"/>
                         <div>
-                            <h1>Nenhuma pergunta por aqui...</h1>
-                            <span>Envie o código desta sala para seus amigos e comece a responder perguntas!</span>
+                            <RoomCode code={params.id}/>
+                            <Button isOutlined onClick={handleEndRoom}>Encerrar Sala</Button>
                         </div>
-                    )}
-                    {questions.map(question => {
-                        return (
-                            <Question
-                                key={question.id}
-                                content={question.content}
-                                author={question.author}
-                                isAnswered={question.isAnswered}
-                                isHighlighted={question.isHighlighted}
-                            >
-                                {!question.isAnswered && (
-                                    <>
-                                        <button
-                                            type="button"
-                                            onClick={()=> handleCheckQuestionAsAnswered(question.id)}
-                                        >
-                                            <img src={checkImg} alt="Marcar pergunta como respondida"/>
-                                        </button>
-                                        <button
-                                            type="button"
-                                            onClick={()=> handleHighlightQuestion(question.id)}
-                                        >
-                                            <img src={answerImg} alt="Dar destaque à pergunta"/>
-                                        </button>
-                                    </>
-                                )}
-                                <button
-                                    type="button"
-                                    onClick={()=> handleDeleteQuestion(question.id)}
-                                >
-                                    <img src={deleteImg} alt="Remover pergunta"/>
-                                </button>
-                            </Question>
-                        );
-                    })}
+                    </div>
+                </header>
+                <div className="loading">
+                    <SyncLoader 
+                        size={15}
+                        color={"#F37A24"}
+                        loading={loading}
+                    />
+                    <h3>Carregando...</h3>
+                    <span>"O único homem que está isento de erros, é aquele que não arrisca acertar." - Albert Einstein</span>
                 </div>
-            </main>
+            </>
+        :
+            <>
+                <header>
+                    <div className="content">
+                        <img src={logoImg} alt="Letmeask"/>
+                        <div>
+                            <RoomCode code={params.id}/>
+                            <Button isOutlined onClick={handleEndRoom}>Encerrar Sala</Button>
+                        </div>
+                    </div>
+                </header>
+                <main className="content">
+                    <div className="room-title">
+                        <h1>Sala {title}</h1>
+                        {questions.length > 0 && <span>{questions.length} perguntas</span>}
+                    </div>
+                    <div className="question-list">
+                        {questions.length === 0 && (
+                            <div>
+                                <h1>Nenhuma pergunta por aqui...</h1>
+                                <span>Envie o código desta sala para seus amigos e comece a responder perguntas!</span>
+                            </div>
+                        )}
+                        {questions.map(question => {
+                            return (
+                                <Question
+                                    key={question.id}
+                                    content={question.content}
+                                    author={question.author}
+                                    isAnswered={question.isAnswered}
+                                    isHighlighted={question.isHighlighted}
+                                >
+                                    {!question.isAnswered && (
+                                        <>
+                                            <button
+                                                type="button"
+                                                onClick={()=> handleCheckQuestionAsAnswered(question.id)}
+                                            >
+                                                <img src={checkImg} alt="Marcar pergunta como respondida"/>
+                                            </button>
+                                            <button
+                                                type="button"
+                                                onClick={()=> handleHighlightQuestion(question.id)}
+                                            >
+                                                <img src={answerImg} alt="Dar destaque à pergunta"/>
+                                            </button>
+                                        </>
+                                    )}
+                                    <button
+                                        type="button"
+                                        onClick={()=> handleDeleteQuestion(question.id)}
+                                    >
+                                        <img src={deleteImg} alt="Remover pergunta"/>
+                                    </button>
+                                </Question>
+                            );
+                        })}
+                    </div>
+                </main>
+            </>
+        }
         </div>
     )
 }
